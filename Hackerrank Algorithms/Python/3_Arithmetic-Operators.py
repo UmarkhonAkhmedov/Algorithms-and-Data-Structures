eadab76917e96f2168a36edb926b7950cce8166e
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-05-08 14:27:13 +0500

    Hackerrank Python exercises from 1 to 5

---
+++ Hackerrank Algorithms/Python/3_Arithmetic-Operators.py
@@ -0,0 +1,10 @@
+num1 = int(input())
+num2 = int(input())
+
+operation1 = num1 + num2
+operation2 = num1 - num2
+operation3 = num1 * num2
+
+print(operation1)
+print(operation2)
+print(operation3)
---
+++ Hackerrank Algorithms/Python/4_Python-Division.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+
+operation1 = num1 // num2
+operation2 = num1 / num2
+
+print(operation1)
+print(operation2)

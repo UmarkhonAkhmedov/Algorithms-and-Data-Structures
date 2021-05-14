6f8121729371b5e90f6a872329ce20a682005354
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-05-14 06:06:39 +0500

    Create Day_2.py
    Challenge Day 2

---
+++ Hackerrank Algorithms/30-Day-Challenge/Day_2.py
@@ -0,0 +1,15 @@
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost / 100 * tip_percent
+    tax = tax_percent/100 * tip_percent
+    total_cost = meal_cost + tip + tax
+    print(round(total_cost))
+
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)

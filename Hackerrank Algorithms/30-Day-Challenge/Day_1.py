988923c72ae2165803fcfb2508441431b4540029
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-05-14 06:05:05 +0500

    Create Day_1.py
    Challenge Day 1

---
+++ Hackerrank Algorithms/30-Day-Challenge/Day_1.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+a = int(input())
+b = float(input())
+c = str(input())
+
+s = 'HackerRank'
+addition = int(a + i)
+anotherAddition = b + d
+print(addition)
+print(anotherAddition)
+print("{} {}".format(s, c))

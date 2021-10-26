9207061851f4999da188576db7fd7a00c489a29c
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-26 08:35:06 +0500

    Solving counting sundays problem

---
+++ Euler Algorithm Problems in JavaScript/18-countingArrays.js
@@ -0,0 +1,11 @@
+function solution() {
+  var n = 0;
+  for (var y = 1901; y <= 2000; y++) {
+    for (var m = 0; m < 12; m++) {
+      if (new Date(y, m, 1).getDay() === 0) {
+        n++;
+      }
+    }
+  }
+  return n;
+}
